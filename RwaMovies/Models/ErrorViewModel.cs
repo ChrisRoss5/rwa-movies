c91af2d23e701bc81977506538f01a270323f78e
KristijanRoss <kristijan.ros@gmail.com>
2023-05-13 21:08:58 +0200

    Add project files.

---
+++ RwaMovies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RwaMovies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
